a73127d47550dc05a740c7c5f6d8173ab548b307
Climer00 <Climer00@gmail.com>
2026-09-22 15:44:09 -0500

    chip art v3 approved: feint

---
+++ sprites-chips-feint.js
@@ -0,0 +1,2 @@
+window.AETHERUM_SPRITES=window.AETHERUM_SPRITES||{};
+window.AETHERUM_SPRITES['chip_feint']='data:image/png;base64,'+((window.__CHIP_PARTS&&window.__CHIP_PARTS['chip_feint'])||[]).join('');
