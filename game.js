31b740a280e404c3fce260b2a5e23a270332a898
Climer00 <Climer00@gmail.com>
2026-09-22 13:59:29 -0500

    Restore game.js as chunk assembler (replace placeholder)

--- game.js
+++ game.js
@@ -1,1 +1,6 @@
-PLACEHOLDER_LOAD_FROM_FILE
+(() => {
+"use strict";
+const parts=window.__AETHERUM_CHUNKS||[];
+if(parts.length<3){document.body.insertAdjacentHTML("beforeend","<pre style=\"color:#f88;padding:1rem\">Missing game chunks</pre>");}
+else (0,eval)(parts.join(""));
+})();
