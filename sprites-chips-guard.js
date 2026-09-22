fe52cf700ac9c0bdee25b695a762d737a2c2b81e
Climer00 <Climer00@gmail.com>
2026-09-22 15:43:02 -0500

    chip art v3: chip_guard parts 1-2 + assembler

--- sprites-chips-guard.js
+++ sprites-chips-guard.js
@@ -1,1 +1,2 @@
-PLACEHOLDER_WILL_REPLACE
+window.AETHERUM_SPRITES=window.AETHERUM_SPRITES||{};
+window.AETHERUM_SPRITES['chip_guard']='data:image/png;base64,'+((window.__CHIP_PARTS&&window.__CHIP_PARTS['chip_guard'])||[]).join('');
